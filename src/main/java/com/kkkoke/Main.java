27c1952c1e338ce5cf9ef9f9cc951e21bdcef34f
kkkoke <1120746320@qq.com>
2023-11-17 10:31:03 +0800

    init: 初始化项目

---
+++ src/main/java/com/kkkoke/Main.java
@@ -0,0 +1,12 @@
+package com.kkkoke;
+
+/**
+ * @author KeyCheung
+ * @date 2023/11/17
+ * @desc
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
